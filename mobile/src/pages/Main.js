b72c253b4c796c4138b2aef4e4d00e130198a106
Lucas E S. Silva <leduardo@dcc.local>
2020-01-16 15:05:01 -0300

    inicio mobile

---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+
+function Main() {
+    const [currentRegion, setCurrentRegion] = useState(null);
+
+    useEffect(() => {
+        async function loadInitialPosition() {
+            const { granted } = await requestPermissionsAsync();
+
+            if (granted) {
+                const { coords } = await getCurrentPositionAsync({ enableHighAccuracy: true });
+
+                const { latitude, longitude } = coords;
+
+                setCurrentRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.04,
+                    longitudeDelta: 0.04
+                })
+            }
+        }
+
+        loadInitialPosition();
+    }, []);
+
+    if (!currentRegion) {
+        return null;
+    }
+
+    return (
+        <MapView initialRegion={currentRegion} style={styles.map}>
+            <Marker coordinate={{ latitude: -23.4352089, longitude: -47.4813074}} />
+        </MapView>
+    )
+}
+
+const styles = StyleSheet.create({
+    map: {
+        flex: 1
+    }
+});
+
+export default Main;
