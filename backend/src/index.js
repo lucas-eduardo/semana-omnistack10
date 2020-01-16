8bd2b0570a63542360f7e46440f454fbe9141807
Lucas Eduardo <lucas.eduardo.developer@gmail.com>
2020-01-15 21:19:02 -0300

    Contruindo a Interface Web

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect('', {
+mongoose.connect('mongodb://localhost:27017/week10?readPreference=primary&appname=MongoDB%20Compass&ssl=false', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
 });
 
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
